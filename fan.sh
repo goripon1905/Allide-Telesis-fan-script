5e4b898029f74b834a6bb90e4fa6074852ca0061
goripon1905 <32355270+goripon1905@users.noreply.github.com>
2023-04-27 19:58:32 +0900

    fix

--- fan.sh
+++ fan.sh
@@ -4,10 +4,3 @@ echo 1000 > /sys/class/hwmon/hwmon0/device/fan1_min
 
 echo 64 > /sys/class/hwmon/hwmon0/device/pwm1_auto_point1_pwm
 echo 64 > /sys/class/hwmon/hwmon0/device/pwm1_auto_point2_pwm
-
-#cat /sys/class/hwmon/hwmon0/device/pwm1_auto_channels_temp
-#cat /sys/class/hwmon/hwmon0/device/pwm1_auto_point1_pwm
-#cat /sys/class/hwmon/hwmon0/device/pwm1_auto_point2_pwm
-#cat /sys/class/hwmon/hwmon0/device/pwm1_enable
-#cat /sys/class/hwmon/hwmon0/device/pwm1_freq
-#cat /sys/class/hwmon/hwmon0/device/pwm1
